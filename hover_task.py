7ed2fd7b257dde6866dd1d155ea90f99f9c8b359
Jinwoo Baek <baek32jinoo@gmail.com>
2018-05-11 23:31:22 -0700

    Create separate hover task

--- hover_task.py
+++ hover_task.py
@@ -2,10 +2,10 @@ import numpy as np
 from physics_sim import PhysicsSim
 from collections import deque
 
-class Task():
+class TaskHover():
     """Task (environment) that defines the goal and provides feedback to the agent."""
     def __init__(self, init_pose=None, init_velocities=None, 
-        init_angle_velocities=None, runtime=5., target_pos=None):
+        init_angle_velocities=None, runtime=5., target_state=None):
         """Initialize a Task object.
         Params
         ======
@@ -13,41 +13,44 @@ class Task():
             init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
             init_angle_velocities: initial radians/second for each of the three Euler angles
             runtime: time limit for each episode
-            target_pos: target/goal (x,y,z) position for the agent
+            target_state: target/goal (x,y,z, 'phi', 'theta', 'psi', 'x_velocity',
+            'y_velocity', 'z_velocity', 'phi_velocity', 'theta_velocity',
+            'psi_velocity') state for the agent
         """
         # Simulation
         self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
         self.action_repeat = 3
 
-        self.state_size = self.action_repeat * 6
+        self.single_state_size = 12
+        self.state_size = self.action_repeat * self.single_state_size
         self.action_low = 0
         self.action_high = 900
         self.action_size = 4
 
-        self.tmp_scores = deque()
-
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
+        self.target_state = target_state if target_state is not None else np.array([0., 0., 10., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
+
+    def get_current_state(self):
+        return np.array(list(self.sim.pose) + list(self.sim.v) + list(self.sim.angular_v))
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        reward = 1.-.3*(abs(self.get_current_state() - self.target_state)).sum()
         return reward
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
         reward = 0
-        pose_all = []
+        state_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
             reward += self.get_reward()
-            pose_all.append(self.sim.pose)
-        next_state = np.concatenate(pose_all)
+            state_all.append(self.get_current_state())
+        next_state = np.concatenate(state_all)
         return next_state, reward, done
 
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat)
+        state = np.concatenate([self.get_current_state()] * self.action_repeat)
         return state
-
